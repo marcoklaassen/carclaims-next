946f03d04537e39ba57e625d5a75d84348262ba5
Flobibbit <floneuss99@gmail.com>
2025-07-07 12:58:17 +0200

    Add OpenAPI generator CLI command and dependency to package.json and generate api stubs

---
+++ openapi/generated/models/index.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Claimsdata';
+export * from './Claimsdetails';
+export * from './ErrorList';
+export * from './ErrorListErrorsInner';
+export * from './Person';
+export * from './Policyholder';
+export * from './VehicleDriver';
+export * from './VehicleDriverDamagedCarImagesInner';
+export * from './VehicleDriverDamagedWindowImagesInner';
+export * from './Witness';

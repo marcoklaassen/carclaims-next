9b6448eeebfffadedc541e74247be4dd19a01ce7
Flobibbit <floneuss99@gmail.com>
2025-07-09 09:24:57 +0200

    add tabname

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import { FormProgressProvider } from "@/context/FormProgressContext";
 import Header from "../../components/header";
 import "./globals.css";
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'CarClaims - Schadensmeldung',
+  description: 'Digitale Schadensmeldung für Kfz-Versicherungen'
+};
 
 export default function RootLayout({
   children,

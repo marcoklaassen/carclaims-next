377bc22fef368d0f87c823679af38632364b0f23
Flobibbit <floneuss99@gmail.com>
2025-07-09 09:50:08 +0200

    fix favicon path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,17 @@
-import { FormProgressProvider } from "@/context/FormProgressContext";
-import Header from "../../components/header";
-import "./globals.css";
+import { FormProgressProvider } from '@/context/FormProgressContext';
+import Header from '../../components/header';
+import './globals.css';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'CarClaims - Schadensmeldung',
   description: 'Digitale Schadensmeldung für Kfz-Versicherungen',
   icons: {
-    icon: '/frida.ico'}
+    icon: '/frida.ico',
+  },
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>

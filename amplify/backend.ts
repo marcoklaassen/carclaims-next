eba5956a9d8fd6fa28864eae087906c7bad6eef8
Flobibbit <floneuss99@gmail.com>
2025-10-20 14:23:00 +0200

    delete unused code

--- amplify/backend.ts
+++
@@ -1,6 +0,0 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { handler as verifyTokenFunction } from './functions/verify-token/resource';
-
-export const backend = defineBackend({
-  verifyTokenFunction,
-});
--- amplify/functions/verify-token/resource.ts
+++
@@ -1,9 +0,0 @@
-import { defineFunction, secret } from '@aws-amplify/backend';
-
-export const handler = defineFunction({
-  entry: './handler.ts',
-  name: 'verifyTokenFunction',
-  environment: {
-    JWT_SECRET: secret("JWT_SECRET"),
-  },
-});
